29eaf57bc4fe92f6740bb9584faa68c2a768a632
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-05-11 12:47:07 +0200

    chore: fixed netlify secrethub usage

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -9,7 +9,7 @@ wget https://github.com/secrethub/secrethub-cli/releases/download/v0.37.0/secret
 tar -zxvf secrethub-v0.37.0-linux-amd64.tar.gz -C ./secrethub
 chmod +x secrethub/bin/secrethub
 rm secrethub-v0.37.0-linux-amd64.tar.gz
-# export PATH="$PWD/secrethub/bin:$PATH"
+export PATH="$PWD/secrethub/bin:$PATH"
 
 echo "--------------------------------------------------"
 echo "2 - Build"
