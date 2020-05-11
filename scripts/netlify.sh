6263d465f214913471c0c843becf2fd894d1701f
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-05-11 12:49:08 +0200

    chore: update wget to be quitt

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -5,7 +5,7 @@ echo "1 - Install SecretHub"
 echo "--------------------------------------------------"
 
 mkdir -p ./secrethub
-wget https://github.com/secrethub/secrethub-cli/releases/download/v0.37.0/secrethub-v0.37.0-linux-amd64.tar.gz
+wget --quiet https://github.com/secrethub/secrethub-cli/releases/download/v0.37.0/secrethub-v0.37.0-linux-amd64.tar.gz
 tar -zxvf secrethub-v0.37.0-linux-amd64.tar.gz -C ./secrethub
 chmod +x secrethub/bin/secrethub
 rm secrethub-v0.37.0-linux-amd64.tar.gz
