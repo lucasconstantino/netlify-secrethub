1a0a224caad2524c27dab0dca6ee94e42ffe7a7d
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-05-11 12:44:31 +0200

    chore: added build script

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-# 1. Install SecretHub
-  mkdir -p ./secrethub
-  wget https://github.com/secrethub/secrethub-cli/releases/download/v0.37.0/secrethub-v0.37.0-linux-amd64.tar.gz
-  tar -zxvf secrethub-v0.37.0-linux-amd64.tar.gz -C ./secrethub
-  chmod +x secrethub/bin/secrethub
-  rm secrethub-v0.37.0-linux-amd64.tar.gz
-  export PATH="$PWD/secrethub/bin:$PATH"
+echo "--------------------------------------------------"
+echo "1 - Install SecretHub"
+echo "--------------------------------------------------"
+
+mkdir -p ./secrethub
+wget https://github.com/secrethub/secrethub-cli/releases/download/v0.37.0/secrethub-v0.37.0-linux-amd64.tar.gz
+tar -zxvf secrethub-v0.37.0-linux-amd64.tar.gz -C ./secrethub
+chmod +x secrethub/bin/secrethub
+rm secrethub-v0.37.0-linux-amd64.tar.gz
+# export PATH="$PWD/secrethub/bin:$PATH"
+
+echo "--------------------------------------------------"
+echo "2 - Build"
+echo "--------------------------------------------------"
+
+secrethub run -- yarn build
