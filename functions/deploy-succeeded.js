7cd1d9438bb08db8db1f15819cdec9a7c1dbab61
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-05-16 02:10:21 +0200

    chore: added workflow dispatching

--- functions/deploy-succeeded.js
+++ functions/deploy-succeeded.js
@@ -1,6 +1,31 @@
-exports.handler = function (event, context, callback) {
-  console.log(process.env.SECRETHUB_ENV, event, process.env);
+const https = require("https");
 
-  // your server-side functionality
-  callback(null, { statusCode: 200, body: "success" });
+exports.handler = function (event, context, callback) {
+  const body = JSON.parse(event.body);
+  const { user, repo } = body.payload.commit_url.match(
+    /github\.com\/(?<user>[^/]+)\/(?<repo>[^/]+)/u
+  ).groups;
+
+  const url = `https://api.github.com/repos/${user}/${repo}/dispatches`;
+  const payload = {
+    event_type: "deploy-succeeded",
+    client_payload: body.payload,
+  };
+
+  const data = JSON.stringify(payload);
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Content-Length": data.length,
+      Authorization: `token ${process.env.GITHUB_OAUTH_TOKEN}`,
+    },
+  };
+
+  const req = https.request(url, options, (res) => callback(null, res));
+
+  req.on("error", (error) => callback(Error(error)));
+
+  req.write(data);
+  req.end();
 };
