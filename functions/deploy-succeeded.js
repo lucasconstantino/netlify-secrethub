246f3e75a3a6a4f2def18653fc2074cc78051981
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-05-16 02:58:27 +0200

    feat: use octokit

--- functions/deploy-succeeded.js
+++ functions/deploy-succeeded.js
@@ -1,43 +1,16 @@
-const https = require("https");
+const { Octokit } = require("@octokit/rest");
+const octokit = new Octokit({ auth: process.env.GITHUB_API_TOKEN });
 
-exports.handler = function (event, context) {
+exports.handler = async function (event) {
   const body = JSON.parse(event.body);
-  const { user, repo } = body.payload.commit_url.match(
-    /github\.com\/(?<user>[^/]+)\/(?<repo>[^/]+)/u
+  const { owner, repo } = body.payload.commit_url.match(
+    /github\.com\/(?<owner>[^/]+)\/(?<repo>[^/]+)/u
   ).groups;
 
-  const payload = {
+  return octokit.repos.createDispatchEvent({
+    owner,
+    repo,
     event_type: "deploy_succeeded",
     client_payload: body.payload,
-  };
-
-  const data = JSON.stringify(payload);
-
-  const options = {
-    port: 443,
-    hostname: "api.github.com",
-    path: `repos/${user}/${repo}/dispatches`,
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Content-Length": Buffer.byteLength(data),
-      Authorization: `token ${process.env.GITHUB_API_TOKEN}`,
-    },
-  };
-
-  console.log({ options });
-
-  const onError = (error) => {
-    console.error(error);
-    context.done(null, "failed");
-  };
-
-  const req = https.request(options, (res) => {
-    res.on("error", onError);
-    res.on("end", () => context.succeed());
   });
-
-  req.on("error", onError);
-  req.write(data);
-  req.end();
 };
